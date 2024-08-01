65d993970912daa9350f8c02617357a69b9bbca1
KaidRommel <18939774762@163.com>
2024-08-01 21:48:19 +0800

    env init

---
+++ drivers_rust/i2c-bcm2835.rs
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+
+//! Rust i2c driver.
+use kernel::prelude::*;
+
+module! {
+    type: IIC-BCM2835-Rust,
+    name: "i2c_bcm2835_rust",
+    author: "Rust for Linux Contributors",
+    description: "Rust printing macros sample",
+    license: "GPL",
+}
+
