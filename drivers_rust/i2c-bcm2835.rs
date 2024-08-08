1cff5be2a08b69f590c764fe910684b06655ae9c
KaidRommel <18939774762@163.com>
2024-08-08 14:04:12 +0800

    backup

--- drivers_rust/i2c-bcm2835.rs
+++ drivers_rust/i2c-bcm2835.rs
@@ -2,7 +2,7 @@
 
 //! Rust i2c driver.
 use kernel::{platform, module_driver, module_platform_driver, of, pr_info};
-use kernel::{define_of_id_table, module_of_id_table, driver_of_id_table, module_id_table};
+use kernel::{define_of_id_table, module_of_id_table, driver_of_id_table,module_id_table};
 
 module_platform_driver! {
     type: I2cBcm2835,
@@ -15,17 +15,19 @@ struct I2cBcm2835;
 
 define_of_id_table! {MY_ID_TABLE, (), [
     (of::DeviceId::Compatible(b"brcm,bcm2711-i2c"), None),
-    (of::DeviceId::Compatible(b"test-device3"), None),
+    (of::DeviceId::Compatible(b"brcm,bcm2835-i2c"), None),
 ]}
 
 module_of_id_table!(MOD_TABLE, MY_ID_TABLE);
 
-driver_of_id_table!(MY_ID_TABLE);
-
-impl platform::Driver for MyDriver {
+impl platform::Driver for I2cBcm2835 {
     driver_of_id_table!(MY_ID_TABLE);
     fn probe(_dev: &mut platform::Device, _id_info: Option<&Self::IdInfo>) -> Result {
         pr_info!("i2c probe!");
+        Ok(())
+    }
+    fn remove(_data: &Self::Data) -> Result {
+        // TODO: remove i2c driver
         Ok(())
     }
 }
